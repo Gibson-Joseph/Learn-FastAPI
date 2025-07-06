97bb2f5c086bb41a95376fdb1fe5675994b177f0
gibson <gibsonjoseph890@gmail.com>
2025-07-06 12:28:41 +0530

    refactor: remove unwanted files and code

--- src/db/main.py
+++ src/db/main.py
@@ -1,8 +1,7 @@
-from sqlmodel import create_engine, text, SQLModel
-from sqlalchemy.ext.asyncio import AsyncEngine
-
-from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import SQLModel, create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src.config import Config
 
@@ -11,9 +10,6 @@ engine = AsyncEngine(create_engine(url=Config.DATABASE_URL, echo=True))
 
 async def init_db():
     async with engine.begin() as conn:
-        # statement = text("SELECT 'hello';")
-        # result = await conn.execute(statement)
-        # print(result.all())
         from src.books.models import Book
 
         await conn.run_sync(SQLModel.metadata.create_all)
