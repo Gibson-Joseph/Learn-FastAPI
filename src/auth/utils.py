a041755e0f44eaf8b7f4d8930e965ab61bc4ae17
gibson <gibsonjoseph890@gmail.com>
2025-08-07 01:23:25 +0530

    JWT authentication

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,4 +1,12 @@
+import jwt
+import uuid
+import logging
+
+from src.config import Config
 from passlib.context import CryptContext
+from datetime import timedelta, datetime
+
+ACCESS_TOKEN_EXPIRY = 3600
 
 passwd_context = CryptContext(schemes=["bcrypt"])
 
@@ -10,3 +18,33 @@ def generate_passwd_hash(password: str) -> str:
 
 def verify_password(password: str, hash: str) -> bool:
     return passwd_context.verify(password, hash)
+
+
+def create_access_token(
+    user_data: dict, expiry: timedelta = None, refresh: bool = False
+):
+    payload = {}
+
+    payload["user"] = user_data
+    payload["exp"] = datetime.now() + (
+        expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY)
+    )
+    payload["jti"] = str(uuid.uuid4())
+    payload["refresh"] = refresh
+
+    token = jwt.encode(
+        payload=payload, key=Config.JWT_SECRET, algorithm=Config.JWT_ALGORITHM
+    )
+
+    return token
+
+
+def decode_token(token: str) -> dict:
+    try:
+        token_data = jwt.decode(
+            jwt=token, key=Config.JWT_SECRET, algorithms=[Config.JWT_ALGORITHM]
+        )
+        return token_data
+    except jwt.PyJWTError as e:
+        logging.exception(e)
+        return None
